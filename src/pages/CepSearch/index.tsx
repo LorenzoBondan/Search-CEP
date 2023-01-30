8cf89129e7817c18206f4add6a1d52157e6898b6
LorenzoBondan <lbbondan@gmail.com>
2023-01-30 16:14:12 -0300

    Events created

--- src/pages/CepSearch/index.tsx
+++ src/pages/CepSearch/index.tsx
@@ -3,17 +3,30 @@ import './styles.css';
 import ResultCard from 'components/ResultCard';
 
 const CepSearch = () => {
+
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => { // copiado ao passar o mouse em cima de onChange -> ChangeEvent sem o handler
+    // sempre que mudar o conteúdo do input
+    console.log("Mudou para: " + event.target.value);
+  }
+
+  const handleSubmit = (event : React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); // evita que a página seja recarregada ao enviar o form
+    console.log("enviou");
+  }
+
+  //onSubmit -> enviar o formulário
   return (
     <div className="cep-search-container">
       <h1 className="text-primary">Busca CEP</h1>
       <div className="container search-container">
-        <form>
+        <form onSubmit={handleSubmit}> 
           <div className="form-container">
             <input
               type="text"
               className="search-input"
               placeholder="CEP (somente números)"
-              onChange={() => {}}
+              onChange={(handleChange)}
             />
             <button type="submit" className="btn btn-primary search-button">
               Buscar
